61b465de29ba81c5f4e9378c9fe95d64f1bc7782
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-05-26 17:17:21 +0300

    dsa

---
+++ src/data.js
@@ -0,0 +1,11 @@
+
+export const refs = {
+
+    pixabayKey: '36775242-4d09ae0ae77a78f3b8d2db4c6',
+    formEl: document.querySelector('.search-form'),
+    galleryEl: document.querySelector('.gallery'),
+    //const moreBtnEl = document.querySelector('.load-more');
+    PER_PAGE: 4,
+    lightbox: new SimpleLightbox('.gallery a')
+    
+    }
---
+++ src/index.js
@@ -0,0 +1,97 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { refs } from './data.js';
+import { createMarkup } from './functions.js'
+
+let pageParams = 1;
+let isCanLoad = false;
+let searchVal;
+
+refs.formEl.addEventListener('submit', function (event) {
+  
+    event.preventDefault();
+
+    refs.galleryEl.innerHTML = '';
+    
+    searchVal = event.target.searchQuery.value;
+
+    if (searchVal.trim() == "") {
+        Notiflix.Report.failure("Input can't be empty!!!");
+        return 
+    }
+
+    pageParams = 1;
+
+    axios({
+        method: 'get',
+        url: 'https://pixabay.com/api/',
+        params: {
+            key: refs.pixabayKey,
+            q: searchVal,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: refs.PER_PAGE,
+            page: pageParams
+        },   
+    })
+        .then(function (response) {
+            if (response.data.hits.length == 0) { 
+                
+                Notiflix.Report.info('Sorry, there are no images matching your search query. Please try again.')
+                return false
+            }
+            
+            isCanLoad = response.data.totalHits > refs.PER_PAGE ? true : false;
+            
+            createMarkup(response.data.hits);
+            
+            refs.lightbox.refresh(); 
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    });
+
+window.addEventListener('scroll', function () {
+ 
+  if((document.body.offsetHeight-window.innerHeight) <= window.pageYOffset && isCanLoad == true){
+    loadMoreImgs();
+  }
+})
+
+function loadMoreImgs() {
+    
+  pageParams += 1;
+
+  axios({
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+          key: refs.pixabayKey,
+          q: searchVal,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: refs.PER_PAGE,
+          page: pageParams
+      }  
+  })
+  .then(function (response) {
+      
+    createMarkup(response.data.hits);
+       
+        refs.lightbox.refresh(); 
+
+      if ( pageParams == Math.ceil(response.data.totalHits / refs.PER_PAGE) ) { 
+          Notiflix.Report.info("We're sorry, but you've reached the end of search results.");
+      }
+  })
+  
+  .catch(function (error) {
+      console.log(error);
+  })
+}
